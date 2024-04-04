ca1409e4e1b9a2091020660549f6f495ffd82b35
Atharvalearner <atharvabanchhode09@gmail.com>
2024-04-04 21:45:38 +0530

    Store merging with existing app

--- app/src/main/java/com/example/newsports/Fragments/Store.java
+++ app/src/main/java/com/example/newsports/Fragments/Store.java
@@ -1,66 +1,139 @@
 package com.example.newsports.Fragments;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Toast;
 import com.example.newsports.R;
+import com.example.newsports.Adapter.HomeAdapter;
+import com.example.newsports.Adapter.PopularAdapter;
+import com.example.newsports.Adapter.RecommendedAdapter;
+import com.example.newsports.Models.HomeCategory;
+import com.example.newsports.Models.PopularModel;
+import com.example.newsports.Models.RecommendedModel;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+import com.google.firebase.storage.FirebaseStorage;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Store#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+
 public class Store extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    FirebaseFirestore firebaseFirestore;
+    RecyclerView popularRec,homeCatRec,recommendedRec;
+    FirebaseStorage db;
 
-    public Store() {
-        // Required empty public constructor
-    }
+    //popular items
+    List<PopularModel> popularModelList;
+    PopularAdapter popularAdapter;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Store.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Store newInstance(String param1, String param2) {
-        Store fragment = new Store();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    //home category
+    List<HomeCategory> categoryList;
+    HomeAdapter homeAdapter;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    //recommendation
+    List<RecommendedModel> recommendedModelList;
+    RecommendedAdapter recommendedAdapter;
 
+
+    @SuppressLint("MissingInflatedId")
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        View root=inflater.inflate(R.layout.fragment_store, container, false);
+
+        firebaseFirestore = FirebaseFirestore.getInstance();
+        //popular item
+
+        popularRec=root.findViewById(R.id.pop_rec);
+        homeCatRec=root.findViewById(R.id.explore_rec);
+        recommendedRec=root.findViewById(R.id.recommended_rec);
+
+
+        popularRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        popularModelList=new ArrayList<>();
+        popularAdapter=new PopularAdapter(getActivity(),popularModelList);
+        popularRec.setAdapter(popularAdapter);
+
+        firebaseFirestore.collection("Users")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                PopularModel popularModel=document.toObject(PopularModel.class);
+                                popularModelList.add(popularModel);
+                                popularAdapter.notifyDataSetChanged();}
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        //explore/home category
+        homeCatRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        categoryList=new ArrayList<>();
+        homeAdapter=new HomeAdapter(getActivity(),categoryList);
+        homeCatRec.setAdapter(homeAdapter);
+
+        firebaseFirestore.collection("HomeCategory")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                HomeCategory homeCategory=document.toObject(HomeCategory.class);
+                                categoryList.add(homeCategory);
+                                homeAdapter.notifyDataSetChanged();}
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+        //recommendation
+        recommendedRec.setLayoutManager(new LinearLayoutManager(getActivity(),RecyclerView.HORIZONTAL,false));
+        recommendedModelList=new ArrayList<>();
+        recommendedAdapter=new RecommendedAdapter(getActivity(),recommendedModelList);
+        recommendedRec.setAdapter(recommendedAdapter);
+
+        firebaseFirestore.collection("recommendation")
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                RecommendedModel recommendedModel =document.toObject(RecommendedModel.class);
+                                recommendedModelList.add(recommendedModel);
+                                recommendedAdapter.notifyDataSetChanged();}
+                        } else {
+                            Toast.makeText(getActivity(), "Error"+task.getException(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+        return root;
+
     }
 }
